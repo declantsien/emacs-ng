0b2464a946e39b29f736af6a2a4252a1a71cfad0
Declan Tsien <declantsien@riseup.net>
2022-12-31 19:35:33 +0800

    webrender: use plain select
    Drop tokio dependency, stick with pselect. Async IO doesn't really fit
    in this case.

--- rust_src/crates/webrender/src/event_loop.rs
+++ rust_src/crates/webrender/src/event_loop.rs
@@ -1,4 +1,11 @@
-use std::{cell::RefCell, ptr, sync::Mutex, time::Instant};
+use errno::{set_errno, Errno};
+use nix::sys::signal::{self, Signal};
+use std::{
+    cell::RefCell,
+    ptr,
+    sync::Mutex,
+    time::{Duration, Instant},
+};
 
 #[cfg(target_os = "macos")]
 use copypasta::osx_clipboard::OSXClipboardContext;
@@ -13,7 +20,6 @@ use copypasta::{
 
 use libc::{c_void, fd_set, pselect, sigset_t, timespec};
 use once_cell::sync::Lazy;
-use tokio::{runtime::Runtime, time::Duration};
 #[cfg(all(feature = "wayland", not(any(target_os = "macos", windows))))]
 use winit::platform::unix::EventLoopWindowTargetExtUnix;
 use winit::{
@@ -29,9 +35,7 @@ use surfman::Connection;
 use surfman::SurfaceType;
 use webrender_surfman::WebrenderSurfman;
 
-use emacs::bindings::{inhibit_window_system, thread_select};
-
-use crate::future::tokio_select_fds;
+use emacs::bindings::{inhibit_window_system, make_timespec, thread_select};
 
 pub type GUIEvent = Event<'static, i32>;
 
@@ -171,20 +175,9 @@ pub static EVENT_LOOP: Lazy<Mutex<WrEventLoop>> = Lazy::new(|| {
     })
 });
 
-pub static TOKIO_RUNTIME: Lazy<Mutex<Runtime>> = Lazy::new(|| {
-    Mutex::new(
-        tokio::runtime::Builder::new_multi_thread()
-            .enable_io()
-            .enable_time()
-            .worker_threads(2)
-            .max_blocking_threads(32)
-            .build()
-            .unwrap(),
-    )
-});
-
 pub static EVENT_BUFFER: Lazy<Mutex<Vec<GUIEvent>>> = Lazy::new(|| Mutex::new(Vec::new()));
 
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct FdSet(pub *mut fd_set);
 
 unsafe impl Send for FdSet {}
@@ -198,6 +191,13 @@ impl FdSet {
     }
 }
 
+impl Drop for FdSet {
+    fn drop(&mut self) {
+        self.clear()
+    }
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct Timespec(pub *mut timespec);
 
 unsafe impl Send for Timespec {}
@@ -228,48 +228,14 @@ pub extern "C" fn wr_select1(
 
     let mut event_loop = EVENT_LOOP.lock().unwrap();
 
-    let event_loop_proxy = event_loop.create_proxy();
-
-    let timeout2 = unsafe { Duration::new((*timeout).tv_sec as u64, (*timeout).tv_nsec as u32) };
-    let read_fds = FdSet(readfds);
-    let write_fds = FdSet(writefds);
-    let timeout = Timespec(timeout);
-
-    let (select_stop_sender, mut select_stop_receiver) = tokio::sync::mpsc::unbounded_channel();
-
-    // use tokio to mimic the pselect because it has cross platform supporting.
-    let tokio_runtime = TOKIO_RUNTIME.lock().unwrap();
-    tokio_runtime.spawn(async move {
-        tokio::select! {
-
-            nfds = tokio_select_fds(nfds, &read_fds, &write_fds, &timeout) => {
-                let _ = event_loop_proxy.send_event(nfds);
-            }
-
-            // time out
-            _ = tokio::time::sleep(timeout2) => {
-                read_fds.clear();
-                write_fds.clear();
-
-                let _ = event_loop_proxy.send_event(0);
-            }
-
-            // received stop command from winit event_loop
-            _ = select_stop_receiver.recv() => {
-                read_fds.clear();
-                write_fds.clear();
-
-                let _ = event_loop_proxy.send_event(1);
-            }
-
-        }
-    });
+    let deadline = Instant::now()
+        + unsafe { Duration::new((*timeout).tv_sec as u64, (*timeout).tv_nsec as u32) };
 
     let nfds_result = RefCell::new(0);
 
     // We mush run winit in main thread, because the macOS platfrom limitation.
     event_loop.el.run_return(|e, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
+        control_flow.set_wait_until(deadline);
 
         match e {
             Event::WindowEvent { ref event, .. } => match event {
@@ -285,21 +251,36 @@ pub extern "C" fn wr_select1(
                     EVENT_BUFFER.lock().unwrap().push(e.to_static().unwrap());
 
                     // notify emacs's code that a keyboard event arrived.
-                    unsafe { libc::raise(libc::SIGIO) };
-
-                    // stop tokio select
-                    let _ = select_stop_sender.send(());
+                    match signal::raise(Signal::SIGIO) {
+                        Ok(_) => {}
+                        Err(err) => log::error!("sigio err: {err:?}"),
+                    };
+                    /* Pretend that `select' is interrupted by a signal.  */
+                    set_errno(Errno(libc::EINTR));
+                    debug_assert_eq!(nix::errno::errno(), libc::EINTR);
+                    nfds_result.replace(-1);
+                    control_flow.set_exit();
                 }
                 _ => {}
             },
 
             Event::UserEvent(nfds) => {
                 nfds_result.replace(nfds);
-                *control_flow = ControlFlow::Exit;
+                control_flow.set_exit();
             }
             _ => {}
         };
     });
+    let ret = nfds_result.into_inner();
+    if ret == 0 {
+        let timespec = unsafe { make_timespec(0, 0) };
+        let nfds =
+            unsafe { libc::pselect(nfds, readfds, writefds, _exceptfds, &timespec, _sigmask) };
+        log::trace!("pselect: {nfds:?}");
+        return nfds;
+    }
 
-    return nfds_result.into_inner();
+    log::trace!("winit event run_return: {ret:?}");
+
+    ret
 }
--- rust_src/lib.rs
+++ rust_src/lib.rs
@@ -1,6 +1,4 @@
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
-#[macro_use]
-extern crate emacs;
 
 use emacs::bindings::{main1, terminate_due_to_signal, will_dump_p};
 
