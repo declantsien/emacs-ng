353e7a85fee7520ee729da33483e9d3344dd7976
Declan Tsien <declantsien@riseup.net>
2023-03-07 22:05:00 +0800

    vendor

---
+++ vendor/servo-display-link/src/lib.rs
@@ -0,0 +1,76 @@
+pub mod ios;
+pub mod macos;
+
+// High level bindings are broken on arm64 macOS. Disabling until there's a solution.
+
+/*use thiserror::Error;
+use time_point::TimePoint;
+
+#[cfg(target_os = "ios")]
+use crate::ios::DisplayLink as PlatformDisplayLink;
+#[cfg(target_os = "macos")]
+use crate::macos::DisplayLink as PlatformDisplayLink;
+
+#[derive(Debug, Error)]
+pub enum PauseError {
+    #[error("already paused")]
+    AlreadyPaused,
+}
+
+#[derive(Debug, Error)]
+pub enum ResumeError {
+    #[error("already running")]
+    AlreadyRunning,
+}
+
+/// `DisplayLink` is a timer object used to synchronize drawing with the refresh rate of the
+/// display.
+#[derive(Debug)]
+pub struct DisplayLink(PlatformDisplayLink);
+
+impl DisplayLink {
+    /// Creates a new `DisplayLink` with a callback that will be invoked with the `TimePoint` the
+    /// screen will next refresh.
+    ///
+    /// The returned `DisplayLink` will be in a paused state. Returns `None` if a `DisplayLink`
+    /// could not be created.
+    ///
+    /// ## Panic
+    ///
+    /// If the callback panics, the process will be aborted.
+    pub fn new<F>(callback: F) -> Option<Self>
+    where
+        F: 'static + FnMut(TimePoint) + Send,
+    {
+        PlatformDisplayLink::new(callback).map(DisplayLink)
+    }
+
+    pub fn on_display<F>(display_id: u32, callback: F) -> Option<Self>
+    where
+        F: 'static + FnMut(TimePoint) + Send,
+    {
+        PlatformDisplayLink::on_display(display_id, callback).map(DisplayLink)
+    }
+
+    pub fn set_current_display(&mut self, display_id: u32) {
+        self.0.set_current_display(display_id)
+    }
+
+    /// Returns `true` if the `DisplayLink` is currently paused.
+    pub fn is_paused(&self) -> bool {
+        self.0.is_paused()
+    }
+
+    /// Pauses the `DisplayLink`.
+    ///
+    /// A paused `DisplayLink` will not invoke it's callback. On iOS, it is necessary to pause the
+    /// `DisplayLink` in response to events like backgrounding.
+    pub fn pause(&mut self) -> Result<(), PauseError> {
+        self.0.pause()
+    }
+
+    /// Resumes the `DisplayLink`.
+    pub fn resume(&mut self) -> Result<(), ResumeError> {
+        self.0.resume()
+    }
+}*/
