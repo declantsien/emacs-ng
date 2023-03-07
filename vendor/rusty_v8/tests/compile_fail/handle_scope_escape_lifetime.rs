82e6e74185bbb1a8ad56a7f6385a17c8af1f9b12
Declan Tsien <declantsien@riseup.net>
2023-03-07 22:05:00 +0800

    vendor

---
+++ vendor/rusty_v8/tests/compile_fail/handle_scope_escape_lifetime.rs
@@ -0,0 +1,19 @@
+// Copyright 2019-2020 the Deno authors. All rights reserved. MIT license.
+use rusty_v8 as v8;
+
+pub fn main() {
+  let mut isolate = v8::Isolate::new(mock());
+  let mut scope1 = v8::HandleScope::new(&mut isolate);
+
+  let _local = {
+    let mut scope2 = v8::HandleScope::new(&mut scope1);
+    let mut scope3 = v8::HandleScope::new(&mut scope2);
+    let mut scope4 = v8::EscapableHandleScope::new(&mut scope3);
+    let value = v8::Integer::new(&mut scope4, 42);
+    scope4.escape(value)
+  };
+}
+
+fn mock<T>() -> T {
+  unimplemented!()
+}
